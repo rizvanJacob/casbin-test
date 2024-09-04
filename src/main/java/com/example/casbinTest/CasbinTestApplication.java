4dcdc1022ed60a696e36d9880ca403775413c213
R Jacob <rizzy@live.com.sg>
2024-09-04 22:13:26 +0800

    spring initialize

---
+++ src/main/java/com/example/casbinTest/CasbinTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.casbinTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasbinTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasbinTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/casbinTest/CasbinTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.casbinTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasbinTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
